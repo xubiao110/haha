d48b97d3f96cced4090a7c047306e7d100207c9a
strong <strongflow@163.com>
2018-03-12 22:05:33 +0800

    like python

--- hello.py
+++ hello.py
@@ -1,4 +1,18 @@
-print('zzzzzzzzzzzzzzzz')
+import random
+
+
+def pasd():
+    s = '1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM'
+    p = ''
+    for i in range(16):
+        a = random.randrange(0, len(s))
+        p += s[a]
+    print('update password:%s' % p)
+
+
+pasd()
+
+print('徐彪通知：我是李大钊')
 
 
 
